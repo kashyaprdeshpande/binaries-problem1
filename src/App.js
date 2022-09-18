7b4df99ff28f8b9e586ec4e4ae481d8c2e22c7a2
omkarsbhedodkar <96882179+omkarsbhedodkar@users.noreply.github.com>
2022-09-19 00:08:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from './Components/NavBar';
+import BookSeat from './Components/BookSeat';
+import { Switch } from '@mui/material';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <NavBar/>
+      <BookSeat/>
+      
+        
+       
+       
+     
+      
+    </div>
+  );
+}
+
+export default App;
